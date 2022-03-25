4d9a849b3585f1e445da0b2afb0453f7bc310f69
s-116-12 <s-116-12@gumiforest.ae.krakow.pl>
2022-03-25 16:10:47 +0100

    initial commit for ecommerc project

---
+++ src/main/java/pl/jkanclerz/credit/App.java
@@ -0,0 +1,7 @@
+package pl.jkanclerz.credit;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/pl/jkanclerz/credit/CreditCard.java
@@ -0,0 +1,13 @@
+package pl.jkanclerz.credit;
+
+import java.math.BigDecimal;
+
+public class CreditCard {
+    public void assignLimit(BigDecimal money) {
+
+    }
+
+    public BigDecimal getBalance() {
+        return BigDecimal.valueOf(1000);
+    }
+}
